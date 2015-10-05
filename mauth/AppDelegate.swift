f932287f3c50a7f9748d3deddac732b19643249a
Marat S <remarr@gmail.com>
2015-10-05 21:08:33 +0300

    app enteredBackgroundTimeout

--- mauth/AppDelegate.swift
+++ mauth/AppDelegate.swift
@@ -13,6 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  /// A `7 seconds ago` is a timeout to post notification of `becomeActive`.
+  private let enteredBackgroundTimeout: NSTimeInterval = -7
+  private lazy var enteredBackground: NSDate = NSDate()
+
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     return true
   }
@@ -20,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillResignActive(application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+    enteredBackground = NSDate()
   }
 
   func applicationDidEnterBackground(application: UIApplication) {
@@ -34,13 +40,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationDidBecomeActive(application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 
-    NSNotificationCenter.defaultCenter().postNotificationName(didBecomeActiveNotificationName, object: nil)
+    // App was in background more than `timeout` constant. I.e if -10 < -7.
+    if enteredBackground.timeIntervalSinceNow < enteredBackgroundTimeout {
+      NSNotificationCenter.defaultCenter().postNotificationName(didBecomeActiveNotificationName, object: nil)
+    }
   }
 
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
 
-
 }
-
