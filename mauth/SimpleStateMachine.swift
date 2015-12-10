d33a886eddf0b0a269aa56467e4cc2181d1017f7
Marat S <remarr@gmail.com>
2015-12-10 19:10:04 +0300

    state machine curated conditions

--- mauth/SimpleStateMachine.swift
+++ mauth/SimpleStateMachine.swift
@@ -10,9 +10,39 @@ import Foundation
 
 enum SimpleStateType: Int {
   case Unauthorized, Processing, Authorized
+
+/*
+
+  - запуск
+  - попытка открыть яндекс
+  - попытка кликнуть на рекламу
+  - попытка открыть секьюрный яндекс
+  - успех
+  - ошибка
+  
+  + если открылась реклама
+  + если яндекс
+  + когда загрузилось
+
+*/
 }
 
 class SimpleConnector {
+
+  enum Стейты {
+    case запуск,
+    попытка_открыть_яндекс,
+    попытка_кликнуть_на_рекламу,
+    попытка_открыть_секьюрный_яндекс,
+    успех,
+    ошибка
+  }
+
+  enum Транзишены {
+    case если_открылась_реклама,
+    если_яндекс,
+    когда_загрузилось
+  }
 
   func shouldTransition(from from: SimpleStateType, to: SimpleStateType) -> Bool {
     switch (from, to) {
