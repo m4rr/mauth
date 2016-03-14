c4dbbe276686398ea4fb39a3dae50c5dba296163
Marat S <remarr@gmail.com>
2016-03-14 18:23:48 +0300

    :fire: CaptiveNetwork doesn't work anymore

--- Sources/CarSocialNetwork.swift
+++ Sources/CarSocialNetwork.swift
@@ -7,30 +7,8 @@
 //
 
 import Foundation
-import UIKit
-import SystemConfiguration.CaptiveNetwork
-//import NetworkExtension
 
-@available(iOS, deprecated=9.0)
-class Car {
-
-  func fetchSSIDInfo() -> CFDictionary? {
-    if let
-      interfaces = CNCopySupportedInterfaces() as? [String],
-      interfaceName = interfaces.first,
-      info = CNCopyCurrentNetworkInfo(interfaceName as CFStringRef) {
-        return info
-    }
-    return nil
-  }
-
-  func lol() {
-    if let
-      ssidInfo = fetchSSIDInfo() as? [String: AnyObject],
-      ssid = ssidInfo["SSID"] as? String {
-        print("SSID: \(ssid)")
     } else {
-      print("SSID not found")
     }
   }
 
