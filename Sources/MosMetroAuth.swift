817d66f95675b0ae2d5f9d7bb02f564d09ec1d32
Marat S <remarr@gmail.com>
2016-03-21 18:47:14 +0300

    mosmetro-auth script

---
+++ Sources/MosMetroAuth.swift
@@ -0,0 +1,164 @@
+//
+//  mosmetroauth.swift
+//  mauth
+//
+//  Created by Marat S. on 21/03/16.
+//  Copyright © 2016 m4rr. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import Kanna
+
+//github.com/m4rr/mosmetro-auth/blob/master/metro.py
+
+class MosMetroAuth {
+
+  func syncRequest(
+    method: Alamofire.Method,
+    _ address: String,
+    parameters: [String: AnyObject]? = nil,
+    headers: [String: String]? = nil,
+    cookies:  [NSHTTPCookie]? = nil
+    ) -> (text: String?, response: NSHTTPURLResponse?) {
+
+      let semaphore = dispatch_semaphore_create(0)
+
+      var response: NSHTTPURLResponse?
+      var text: String?
+
+      if let cookies = cookies, url = NSURL(string: address) {
+        Alamofire.Manager.sharedInstance.session.configuration.HTTPCookieStorage?.setCookies(cookies, forURL: url, mainDocumentURL: nil)
+      }
+
+      Alamofire
+        .request(method, address, parameters: parameters, headers: headers)
+        .responseString { rp in
+          response = rp.response
+          text = rp.result.value
+
+          dispatch_semaphore_signal(semaphore)
+      }
+
+      dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+      
+      return (text, response)
+  }
+
+  func tryConnect(address: String) -> Bool {
+    var result = false
+
+    guard let url = NSURL(string: address) else {
+      return result
+    }
+
+    let rq = NSURLRequest(URL: url, cachePolicy: .ReloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 5)
+
+    let semaphore = dispatch_semaphore_create(0)
+
+    NSURLSession.sharedSession().dataTaskWithRequest(rq) { _, _, err in
+      result = err == nil
+
+      dispatch_semaphore_signal(semaphore)
+    }
+
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+
+    return result
+  }
+
+  func connect() {
+    var headersSet: [String: String] = [:]
+
+    let pageVmetro = syncRequest(.GET, "http://vmet.ro")
+    guard let
+      pageVmetroURL = pageVmetro.response?.URL,
+      text = pageVmetro.text,
+      urlAuthStr = matchesForRegexInText("https:[^\"]*", text: text).first,
+      headers1 = pageVmetro.response?.allHeaderFields as? [String : String]
+      else { return }
+    headersSet["referer"] = pageVmetroURL.absoluteString
+
+    let pageAuth = syncRequest(.GET, urlAuthStr,
+      headers: headersSet,
+      cookies: NSHTTPCookie.cookiesWithResponseHeaderFields(headers1, forURL: pageVmetroURL))
+    guard let
+      pageAuthURL = pageAuth.response?.URL,
+      pageAuthText = pageAuth.text,
+      pageAuthBody = matchesForRegexInText("<body>.*?</body>", text: pageAuthText).first,
+      headers2 = pageAuth.response?.allHeaderFields as? [String : String]
+      else { return }
+    headersSet["referer"] = pageAuthURL.absoluteString
+
+    let pagePostAuth = syncRequest(.POST, urlAuthStr,
+      parameters: formInputParse(pageAuthBody),
+      headers: headersSet,
+      cookies: NSHTTPCookie.cookiesWithResponseHeaderFields(headers2, forURL: pageAuthURL))
+    guard let
+      pagePostAuthURL = pagePostAuth.response?.URL,
+      pagePostAuthBody = matchesForRegexInText("<body>.*?</body>", text: pagePostAuth.text).first,
+      headers3 = pagePostAuth.response?.allHeaderFields as? [String : String]
+      else { return }
+    headersSet["referer"] = pagePostAuthURL.absoluteString
+
+    let pageRouter = syncRequest(.POST, "http://1.1.1.1/login.html",
+      parameters: formInputParse(pagePostAuthBody),
+      headers: headersSet,
+      cookies: NSHTTPCookie.cookiesWithResponseHeaderFields(headers3, forURL: pagePostAuthURL))
+  }
+
+  func go() -> NSTimeInterval {
+    let startTime = NSDate()
+
+    if tryConnect("http://1.1.1.1/login.html") {
+      for counter in 0..<3 {
+        if tryConnect("https://wtfismyip.com/text") {
+          if counter == 0 {
+            print("Already connected")
+          } else {
+            print("Connected")
+          }
+
+          break
+        }
+
+        connect()
+      }
+    } else {
+      print("Wrong network")
+    }
+
+    return startTime.timeIntervalSinceNow
+  }
+
+  init() {
+    go()
+  }
+
+  func formInputParse(body: String) -> [String: String] {
+    var data: [String: String] = [:]
+
+    if let doc = Kanna.HTML(html: body, encoding: NSUTF8StringEncoding) {
+      for input in doc.css("input") {
+        data[input["name"]!] = input["value"]
+      }
+    }
+
+    return data
+  }
+
+  func matchesForRegexInText(regex: String!, text: String!) -> [String] {
+    do {
+      let regex = try NSRegularExpression(pattern: regex, options: [])
+      let nsString = text as NSString
+      let results = regex.matchesInString(text,
+        options: [], range: NSMakeRange(0, nsString.length))
+      return results.map { nsString.substringWithRange($0.range)}
+    } catch let error as NSError {
+      print("invalid regex: \(error.localizedDescription)")
+      return []
+    }
+  }
+
+
+}
--- mauthTests/mauthTests.swift
+++ mauthTests/mauthTests.swift
@@ -23,6 +23,16 @@ class mauthTests: XCTestCase {
   func testExample() {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    let mma = MosMetroAuth()
+
+    let html = "<html><head></head><body><ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span><a href='example.com'>example(English)</a><a href='example.co.jp'>example(JP)</a></body>"
+
+    let data = mma.formInputParse(html)
+
+    assert(data.count == 2)
+
+
   }
 
   func testPerformanceExample() {
