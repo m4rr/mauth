c673c0faee4e7fb01d4f22b26753d33031daf77a
Marat S <remarr@gmail.com>
2016-03-22 11:46:20 +0300

    obj-c++

---
+++ Sources/NSObject+MosMetroAuthOCpp.h
@@ -0,0 +1,19 @@
+//
+//  NSObject+MosMetroAuthOCpp.h
+//  mauth
+//
+//  Created by Marat S. on 21/03/16.
+//  Copyright © 2016 m4rr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@class CppObject;
+
+@interface MyClass : NSObject {
+//  CppObject wrapped;
+}
+
+- (void)exampleMethodWithString:(NSString *)str;
+
+@end
