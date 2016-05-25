4290398ba22c11355526d730787f2420a5032876
Marat Saytakov <remarr@gmail.com>
2016-05-25 12:35:06 +0300

    update agent

--- Sources/NeatViewController.swift
+++ Sources/NeatViewController.swift
@@ -90,7 +90,7 @@ class NeatViewController: UIViewController {
     webView = WKWebView(frame: view.bounds, configuration: config)
 
     if #available(iOS 9.0, *) {
-        webView.customUserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E; FerrariGT)" // vertu user-agent :)
+        //webView.customUserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E; FerrariGT)" // vertu user-agent :)
     } else {
         // Fallback on earlier versions
     }
